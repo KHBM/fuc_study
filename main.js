bdc2c988c00903467e558814b6c4ed38f7312529
foxrain <kimilb412@gmail.com>
2021-03-18 14:50:40 +0900

    유사 모나드 두 표기법 살펴보기 only in Maybe.

--- main.js
+++ main.js
@@ -43,3 +43,44 @@ bi = Maybe.just(50)
  .bind( x => Maybe.just(100)
     .bind(y => Maybe.just(200 + x)
         .bind(z => Maybe.just(x + y + z))))
+
+function Do(m, f) {
+  const gen = f();
+
+  function doRec(v = undefined) {
+    const {value, done} = gen.next(v);
+    //const valueM = value instanceof m ? value : m.of(value);
+    //return done ? valueM : valueM.bind(doRec);
+    return done ? value : value.bind(doRec);
+  }
+
+  return doRec();
+} // code from https://gist.github.com/MaiaVictor/bc0c02b6d1fbc7e3dbae838fb1376c8
+
+// and can I apply this mechanism to those binds chain?0
+
+function trys(maybeValue)
+{
+	return Do(Maybe, function*()
+	{
+		let first = yield (x => Maybe.just(100))
+		let second = yield (y => Maybe.just(200 * first))
+		return (z => Maybe.just(first + second + z))
+	})
+}
+//뭔가 이상한데.. first를 더하고자 하는게 아닌데...
+function trys(maybeValue)
+{
+	return Do(Maybe, function*()
+	{
+		let x = yield maybeValue
+		let y = yield Maybe.just(100)
+		let z = yield Maybe.just(200 + x)
+		return Maybe.just(x + y + z)
+	})
+}
+
+
+//이렇게 하면..
+
+bi = trys(pure(50)) // bi.extract() -> 400
